c592b3837203b578defd5a6b390e9214008e3130
zhoufengfan <zhfengfan@126.com>
2021-02-02 23:52:15 +0800

    1454e48f82e279707f33b2872a14857d4b00f726 achieved 0.97

--- use_torchvision_resnet.py
+++ use_torchvision_resnet.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     batch_size = 128
     cycle_epoches_for_test = 2
     os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
     # transform2 = transforms.Compose([
     #     transforms.ToTensor(),
     #     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
